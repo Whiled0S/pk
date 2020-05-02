53f254af0c95f1bccc7725e77cb61388a091ce41
whiled0s <whiled0s@yandex.ru>
2020-05-02 12:43:29 +0300

    Added icons. Removed redundant READMEs. Changed favicon.ico

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'ООО "ПК"',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -43,8 +43,8 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/svg-sprite',
-    '@nuxtjs/svg'
+    '@nuxtjs/svg',
+    '@nuxtjs/svg-sprite'
   ],
 
   styleResources: {
