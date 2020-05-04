7848a6dd358508070d778237a446609feeeded04
whiled0s <whiled0s@yandex.ru>
2020-05-04 12:15:17 +0300

    Temporarily disabled mailer module

--- api/mail.js
+++ api/mail.js
@@ -1,37 +1,38 @@
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
 
 module.exports = function (req, res) {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.yandex.ru',
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'sender@ololocalhost.ru',
-      pass: '58901337Wh'
-    }
-  })
-
-  transporter.verify(function (error) {
-    if (error) {
-      // eslint-disable-next-line no-console
-      console.log(error)
-    } else {
-      // eslint-disable-next-line no-console
-      console.log('Server is ready')
-    }
-  })
-
-  transporter.sendMail({
-    from: 'sender@ololocalhost.ru',
-    to: 'mailer@ololocalhost.ru',
-    subject: 'Hello world',
-    text: 'Plaintext version of the message',
-    html: '<p>Письмишко</p>'
-  }, (err, info) => {
-    if (err) {
-      res.status(500).send(err)
-    } else {
-      res.status(200).send(info)
-    }
-  })
+  res.status(200).send('This is a mailer module')
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.yandex.ru',
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: 'sender@ololocalhost.ru',
+  //     pass: '58901337Wh'
+  //   }
+  // })
+  //
+  // transporter.verify(function (error) {
+  //   if (error) {
+  //     // eslint-disable-next-line no-console
+  //     console.log(error)
+  //   } else {
+  //     // eslint-disable-next-line no-console
+  //     console.log('Server is ready')
+  //   }
+  // })
+  //
+  // transporter.sendMail({
+  //   from: 'sender@ololocalhost.ru',
+  //   to: 'mailer@ololocalhost.ru',
+  //   subject: 'Hello world',
+  //   text: 'Plaintext version of the message',
+  //   html: '<p>Письмишко</p>'
+  // }, (err, info) => {
+  //   if (err) {
+  //     res.status(500).send(err)
+  //   } else {
+  //     res.status(200).send(info)
+  //   }
+  // })
 }
